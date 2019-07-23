f5dc1e82a4560d5357c9de1620a561afe786e425
JoaoV111 <52671234+JoaoV111@users.noreply.github.com>
2019-07-23 16:10:33 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+"""Inicia Programa Controle de Estoque"""
+import config as cfg
+
+print('\033[1;32m\n\n')
+print(f'-' * 30)
+print(f'\n{"Controle de Estoque  V0.2":^30}\n')
+print(f'-' * 30)
+print('\n\n\033[m')
+
+
+while True:
+    cfg.welcome()
+    cfg.waitCommand()
+
+
+
