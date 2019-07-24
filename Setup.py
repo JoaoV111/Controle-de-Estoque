71cd33f404bec74798714cbb8775622a50a914c0
JoaoV111 <52671234+JoaoV111@users.noreply.github.com>
2019-07-23 21:53:13 -0300

    Add files via upload

--- Setup.py
+++ Setup.py
@@ -5,24 +5,26 @@ from sqlalchemy.orm import sessionmaker
 
 # criando db
 # dialect+driver://username:password@host:port/database
-engine = db.create_engine("'postgresql://postgres:666@localhost/controle_de\
-_estoque_db'")
+engine = db.create_engine('postgresql://postgres:666@localhost/controle_de\
+_estoque_db')
 if not database_exists(engine.url):
     create_database(engine.url)
 
 # mapeando db
 Base = declarative_base()
+
+
 class Product(Base):
-     __tablename__ = 'products'
+        __tablename__ = 'products'
 
-     id = db.Column(db.Integer,  db.Sequence('product_id_seq'), primary_key=True)
-     name = db.Column(db.String(255))
-     model = db.Column(db.String(255))
-     quantity = db.Column(db.Integer)
+        id = db.Column(db.Integer, db.Sequence('product_id_seq'), primary_key=True)
+        name = db.Column(db.String(255))
+        model = db.Column(db.String(255))
+        quantity = db.Column(db.Integer)
 
-     def __repr__(self):
-        return "<Product(id='%d', name='%s', model='%s', quantity='%d')>" % (
-                        self.id, self.name, self.model, self.quantity)
+        def __repr__(self):
+                return "<Product(id='%d', name='%s', model='%s', quantity='%d')>" % (
+            self.id, self.name, self.model, self.quantity)
 
 Base.metadata.create_all(engine)
 
@@ -32,13 +34,11 @@ session = Session()
 
 # inserindo dados na tabela
 session.add_all([
-        Product(name = 'Presunto', model = 'Perdigão', quantity = 50),
-        Product(name = 'Queijo', model = 'Perdigão', quantity = 20),
-        Product(name = 'Calabresa', model = 'Perdigão', quantity = 5),
-        Product(name = 'Peito de peru', model = 'Perdigão', quantity = 50),
-        Product(name = 'Salame', model = 'Perdigão', quantity = 42)])
+    Product(name='Presunto', model='Perdigão', quantity=50),
+    Product(name='Queijo', model='Perdigão', quantity=20),
+    Product(name='Calabresa', model='Perdigão', quantity=5),
+    Product(name='Peito de peru', model='Perdigão', quantity=50),
+    Product(name='Salame', model='Perdigão', quantity=42)
+])
 
 session.commit()
-
-
-
